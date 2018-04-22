1d47c41cfd34265a1aba82989d73cef3cf6a90d4
Michal Hozza <mhozza@gmail.com>
2018-04-22 19:19:24 +0100

    Basic bootstrap template.

--- pi_control/home/views.py
+++ pi_control/home/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Hello')
+    return render(request, 'home/home.html', dict())
--- pi_control/pi_control/settings.py
+++ pi_control/pi_control/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'pi_control',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../.static')
