21b61146fe3e75b6ed2d04a5399f05f6e94a2d77
Michal Hozza <mhozza@gmail.com>
2018-05-10 21:10:15 +0100

    Move hi & low constants to server.

--- pi_control/home/js/app.jsx
+++ pi_control/home/js/app.jsx
@@ -3,39 +3,43 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 class TemperatureWidget extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      colorClass: props.value > props.high
-        ? "text-danger"
-        : props.value < props.low
-          ? "text-primary"
-          : "text-success"
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            colorClass: props.value > props.high
+                ? "text-danger"
+                : props.value < props.low
+                    ? "text-primary"
+                    : "text-success"
+        };
+    }
 
-  render() {
-    return (<div className="card">
-      <div className="card-header">{this.props.title}</div>
-      <div className="card-body">
-        <div className={"card-text text-center temperature-widget-body " + this.state.colorClass}>
-          {this.props.value}<sup>
-            <span>{this.props.unit}</span>
-          </sup>
-        </div>
-      </div>
-      <div className="card-footer text-muted text-right">{this.props.time}</div>
-    </div>);
-  }
+    render() {
+        return (<div className="card">
+            <div className="card-header text-center">{this.props.title}</div>
+            <div className="card-body">
+                <div className={"card-text text-center temperature-widget-body " + this.state.colorClass}>
+                    {this.props.value}<sup>
+                    <span>{this.props.unit}</span>
+                </sup>
+                </div>
+            </div>
+            <div className="card-footer text-muted text-center">{this.props.time}</div>
+        </div>);
+    }
 }
 
 function update_temperature() {
-  axios.get("/api/temperature").then(response => {
-    let time = new Date(response.data.time).toLocaleString();
+    axios.get("/api/temperature").then(response => {
+        let time = new Date(response.data.time).toLocaleString();
 
-    ReactDOM.render(<TemperatureWidget title="Teplota" value={response.data.temperature} unit="°C" time={time} high="25" low="23"/>, document.getElementById('temperature_widget'));
-    ReactDOM.render(<TemperatureWidget title="Vlhkosť" value={response.data.humidity} unit="%" time={time} high="50" low="33"/>, document.getElementById('humidity_widget'));
-  });
+        ReactDOM.render(<TemperatureWidget title="Teplota" value={response.data.temperature.value} unit="°C" time={time}
+                                           high={response.data.temperature.high}
+                                           low={response.data.temperature.low}/>, document.getElementById('temperature_widget'));
+        ReactDOM.render(<TemperatureWidget title="Vlhkosť" value={response.data.humidity.value} unit="%" time={time}
+                                           high={response.data.humidity.high}
+                                           low={response.data.humidity.low}/>, document.getElementById('humidity_widget'));
+    });
 }
 
 update_temperature();
--- pi_control/temperature/views.py
+++ pi_control/temperature/views.py
@@ -7,12 +7,26 @@ from django.utils import timezone
 from .measure import measure_temperature_and_humidity
 
 
+HUMIDITY_HIGH = 50
+HUMIDITY_LOW = 30
+TEMPERATURE_HIGH = 25
+TEMPERATURE_LOW = 22
+
+
 @api_view(['GET'])
 def get_temperature_and_humidity(request):
     now = timezone.now()
     temperature, humidity = measure_temperature_and_humidity(settings.DEBUG)
     return Response({
         'time': now,
-        'temperature': temperature,
-        'humidity': humidity,
+        'temperature': {
+            'value': temperature,
+            'high': TEMPERATURE_HIGH,
+            'low': TEMPERATURE_LOW,
+        },
+        'humidity': {
+            'value': humidity,
+            'high': HUMIDITY_HIGH,
+            'low': HUMIDITY_LOW,
+        },
     })
