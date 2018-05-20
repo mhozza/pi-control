4acb13b23403f3337d999da78f50ef6c62a1227f
Michal Hozza <mhozza@gmail.com>
2018-05-20 19:01:48 +0100

    Simplify and fix webpack config.

--- pi_control/home/js/webpack.config.js
+++ pi_control/home/js/webpack.config.js
@@ -1,35 +1,27 @@
-const debug = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 
-module.exports = {
-  context: __dirname,
-  devtool: debug ? 'inline-sourcemap' : false,
-  entry: './app.jsx',
-  output: {
-    path: __dirname + '/../static/js/',
-    filename: debug ? 'app.js' : 'app.min.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
+module.exports = (env, args) => {
+  const debug = args.mode !== 'production';
+  return {
+    context: __dirname,
+    devtool: debug
+      ? 'inline-sourcemap'
+      : false,
+    entry: './app.jsx',
+    output: {
+      path: __dirname + '/../static/js/',
+      filename: 'app.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader'
+          }
         }
-      }
-    ]
-  },
-  plugins: debug ? [] : [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': debug ? JSON.stringify('development') : JSON.stringify('production')
-    }),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: true,
-      sourcemap: false,
-      minimize: true
-    }),
-  ],
+      ]
+    }
+  };
 };
