d860bbb91ba707b98e71f18252711201e8271166
Michal Hozza <mhozza@gmail.com>
2018-05-20 14:36:34 +0100

    Remove erratic measurements.

--- pi_control/temperature/tasks.py
+++ pi_control/temperature/tasks.py
@@ -5,5 +5,9 @@ from .measure import measure_temperature_and_humidity
 
 @shared_task
 def log_temperature():
-    temperature, humidity = measure_temperature_and_humidity()
+    temperature_measurements, humidity_measurements = zip(*[measure_temperature_and_humidity() for i in range(3)])
+
+    temperature = sorted(temperature_measurements)[1]
+    humidity = sorted(humidity_measurements)[1]
+
     Entry.objects.create(temperature=temperature, humidity=humidity)
