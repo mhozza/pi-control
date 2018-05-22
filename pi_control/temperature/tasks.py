8f42cad16012c188c5dbe00ee7e503871eaaef77
Michal Hozza <mhozza@gmail.com>
2018-05-22 22:21:33 +0100

    Deleay 1s between multiple measurements.

--- pi_control/temperature/tasks.py
+++ pi_control/temperature/tasks.py
@@ -1,11 +1,18 @@
+import time
+
 from celery import shared_task
-from .models import Entry
+
 from .measure import measure_temperature_and_humidity
+from .models import Entry
 
 
 @shared_task
 def log_temperature():
-    temperature_measurements, humidity_measurements = zip(*[measure_temperature_and_humidity() for i in range(3)])
+    def delay_measure(delay):
+        time.sleep(delay)
+        return measure_temperature_and_humidity()
+
+    temperature_measurements, humidity_measurements = zip(*[delay_measure(i > 0) for i in range(3)])
 
     temperature = sorted(temperature_measurements)[1]
     humidity = sorted(humidity_measurements)[1]
