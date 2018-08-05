577923dcf2ddb27bdd4259ba6924110e6b69184d
Michal Hozza <mhozza@gmail.com>
2018-08-05 10:40:56 +0100

    Add task for making the computer sleep.

--- pi_control/pc_status/tasks.py
+++ pi_control/pc_status/tasks.py
@@ -1,8 +1,13 @@
 from celery import shared_task
 
-from .pc_status import wakeup
+from .pc_status import wakeup, make_sleep
 
 
 @shared_task
 def wake_pc(mac):
     return wakeup(mac)
+
+
+@shared_task
+def make_sleep(url, secret):
+    return make_sleep(url, secret)
