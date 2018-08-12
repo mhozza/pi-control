3915d4b0f788733804836ce196e7ba162ccd9e8b
Michal Hozza <mhozza@gmail.com>
2018-08-12 15:05:14 +0100

    check if service is running

--- pi_control/server_stats/helpers.py
+++ pi_control/server_stats/helpers.py
@@ -36,3 +36,15 @@ def get_update_counts():
         out = '0;0\n'
     total, security = out.strip().split(';')
     return int(total), int(security)
+
+
+def is_service_active(service):
+    """Return True if service is running"""
+    cmd = '/bin/systemctl status %s.service' % service
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    stdout_list = proc.communicate()[0].split('\n')
+    for line in stdout_list:
+        if 'Active:' in line:
+            if '(running)' in line:
+                return True
+    return False
