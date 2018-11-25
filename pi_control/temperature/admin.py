1a8c3f5bb3dbab3e0a9cc612421892a4f5243f43
Michal Hozza <mhozza@gmail.com>
2018-11-25 20:59:35 +0000

    Add support for managing devices.

--- pi_control/temperature/admin.py
+++ pi_control/temperature/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Entry
+
+from .models import Entry, MeasurementDevice, Room
 
 
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MeasurementDevice)
+class MeasurementDeviceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
     pass
--- pi_control/temperature/models.py
+++ pi_control/temperature/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
 
+class MeasurementDevice(models.Model):
+    id = models.CharField(primary_key=True, max_length=100, verbose_name='device ID')
+    name = models.CharField(max_length=100, verbose_name='device name')
+
+
 class Entry(models.Model):
     time = models.DateTimeField(auto_now_add=True, verbose_name='measurement time.')
     temperature = models.FloatField(verbose_name='temperature in Celsius.')
     humidity = models.FloatField(verbose_name='humidity in %.')
-    device_id = models.CharField(max_length=100, verbose_name='device ID')
+    device_id = models.ForeignKey(MeasurementDevice, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'temperature entry'
@@ -13,3 +18,8 @@ class Entry(models.Model):
 
     def __str__(self):
         return '{}: {}°C, {}%'.format(self.time, self.temperature, self.humidity)
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=100, verbose_name='room name')
+    devices = models.ManyToManyField(MeasurementDevice)
