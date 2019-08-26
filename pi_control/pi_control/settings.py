4c83c8a7a7d06861224dbcfdd11714e6b866387a
Michal Hozza <mhozza@gmail.com>
2019-08-26 15:02:45 +0100

    Use whitenoise to serve staticfiles.

--- pi_control/pi_control/settings.py
+++ pi_control/pi_control/settings.py
@@ -41,6 +41,7 @@ INTERNAL_IPS = ["127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",  # This needs to be first
     "home",
     "network",
     "pc_status",
@@ -70,6 +71,7 @@ if DEBUG:
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -153,9 +155,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "../.static")
+STATIC_ROOT = env("STATIC_ROOT", os.path.join(BASE_DIR, "../.static"))
 if not DEBUG:
-    STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Social Auth
 LOGIN_URL = "login"
