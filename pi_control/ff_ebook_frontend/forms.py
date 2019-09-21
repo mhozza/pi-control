24e53091e1b8a515da17e88dbfbc566dd9bb13c0
Michal Hozza <mhozza@gmail.com>
2019-09-21 13:20:26 +0100

    Add calibre to docker (#15)
    * Add calibre with debian image.
    
    * Dev docker with calibre.
    
    * Allow external mail server configuration via env.
    
    * Production docker with calibre.
    
    * Configure tls via env and update example.env
    
    * Remove sender address from ebook converter.
    
    * Remove calibre installer.

--- pi_control/ff_ebook_frontend/forms.py
+++ pi_control/ff_ebook_frontend/forms.py
@@ -2,6 +2,5 @@ from django import forms
 
 
 class BuildEbookForm(forms.Form):
-    url = forms.URLField(label="Adresa", required=True, initial="http://")
+    url = forms.URLField(label="Adresa", required=True)
     kindle_email = forms.EmailField(label="Adresa čítačky", required=True)
-    sender_email = forms.EmailField(label="Adresa odosielateľa", required=True)
--- pi_control/ff_ebook_frontend/views.py
+++ pi_control/ff_ebook_frontend/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.messages import SUCCESS, add_message
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
@@ -11,13 +12,14 @@ class BuildEbookView(FormView):
     form_class = BuildEbookForm
     success_url = reverse_lazy("build-ebook")
 
-    def get_initial(self):
-        return {"sender_email": self.request.user.email}
+    def get_context_data(self, **kwargs):
+        """Use this to add extra context."""
+        context = super(BuildEbookView, self).get_context_data(**kwargs)
+        context["from_email"] = settings.DEFAULT_FROM_EMAIL
+        return context
 
     def form_valid(self, form):
         url = form.cleaned_data["url"]
-        result = convert_ebook.delay(
-            url, form.cleaned_data["sender_email"], form.cleaned_data["kindle_email"]
-        )
+        convert_ebook.delay(url, form.cleaned_data["kindle_email"])
         add_message(self.request, SUCCESS, "Vyrábam knihu pre Kindle z {}.".format(url))
         return super().form_valid(form)
