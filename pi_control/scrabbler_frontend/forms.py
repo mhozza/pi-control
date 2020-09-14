c04b276af514f58e9a8bc16f5e100f087b9bc263
Michal Hozza <mhozza@gmail.com>
2020-09-14 22:52:35 +0100

    Customize scrabbler form.

--- pi_control/scrabbler_frontend/forms.py
+++ pi_control/scrabbler_frontend/forms.py
@@ -1,5 +1,8 @@
 from enum import Enum
 
+from crispy_forms.bootstrap import InlineRadios
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Div, Field, Layout, Row, Submit
 from django import forms
 from django.conf import settings
 from django.forms import RadioSelect
@@ -25,3 +28,25 @@ class ScrabblerForm(forms.Form):
     prefix = forms.CharField(label="Prefix", max_length=100, required=False)
     use_all_letters = forms.BooleanField(label="Pouzi vsetky pismena", initial=True, required=False)
     wildcard = forms.BooleanField(label="Wildchar", initial=False, required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Div("prefix", css_class="col-md-3"),
+                Div(Field("word", autofocus="true"), css_class="col-md-6"),
+                Div("dictionary", css_class="col-md-3"),
+            ),
+            Row(
+                Div(InlineRadios("mode"), css_class="col-md-3"),
+                Div("limit", css_class="col-md-3"),
+                Div(
+                    Field("use_all_letters", css_class="custom-checkbox-inline"),
+                    css_class="col-md-3",
+                ),
+                Div("wildcard", css_class="col-md-3"),
+                css_class="form-row align-items-center",
+            ),
+            Row(Submit("submit", "Hladaj", css_class="btn btn-success col-md-12")),
+        )
